3a5964713e92c0b0e3bd7dd6fa2237723d89c5f3
Liam <lmmurray@student.fullsail.edu>
2020-05-07 17:13:52 -0400

    adds matrix4 to mat.ts and renames tex2DGrad

---
+++ src/mat.ts
@@ -0,0 +1,14 @@
+export declare type MAT4 = 
+[
+    number, number, number, number,
+    number, number, number, number,
+    number, number, number, number,
+    number, number, number, number,
+];
+
+export const Mat4Identity : MAT4 = [
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1,
+];
